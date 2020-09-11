78319221349f347ab1c2ab2ac743e29650ba34e6
vitaliy-kravchenk0 <vitaliy.blogger@gmail.com>
2020-09-11 16:26:45 +0300

    Добавил крестик, что бы можно было закрыть модальное окно

--- src/js/modals.js
+++ src/js/modals.js
@@ -2,4 +2,7 @@ $(document).ready(function(){
     $(".price__discover-1").click(function(){
         $(".modals").css("display", "flex");
     });
+    $(".close-block").click(function(){
+        $(".modals").css("display", "none");
+    });
 });
